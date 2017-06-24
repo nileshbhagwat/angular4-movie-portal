4b940debc0b2461b83487c73f943365a9c02dd5b
nbhagwat <nilesh7bhagwat@gmail.com>
2017-06-24 18:56:04 +0530

    feat(Loading Screen) Playing with loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(){
+    /* Since loading screen having blue background
+    *  Not proper solution to show loading screen and change body background color :P
+    */
+    let body = document.getElementsByTagName('body')[0];
+    body.style.backgroundColor = "white";
+  }
 }
